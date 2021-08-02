8c1b0a8aafef8dc885dd36250731adb71d9dcf30
Ranjan <ranjankumarjha470@gmail.com>
2021-08-03 02:26:44 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,22 @@
+
+let userInput = document.querySelector(".user-input");
+let chatwindow = document.querySelector(".chat-window");
+let myName = document.querySelector(".me .user-name");
+let onlineList = document.querySelector(".online-list");
+let username = prompt("Enter your name: ");
+myName.textContent = username;
+
+userInput.addEventListener("keypress", function (e) {
+  if (e.key == "Enter") {
+    let chatDiv = document.createElement("div");
+    chatDiv.classList.add("chat");
+    chatDiv.classList.add("right");
+    chatDiv.textContent = username + " : " + userInput.value;
+    chatwindow.append(chatDiv);
+    socket.emit("chat", { username, chat: userInput.value });
+    userInput.value = "";
+
+    chatwindow.scrollTop = chatwindow.scrollHeight;
+
+  }
+})
