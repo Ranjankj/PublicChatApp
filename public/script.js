f7835d2276be06d5a7a79d0a9ec659a112072d92
Ranjan <ranjankumarjha470@gmail.com>
2021-08-03 19:18:11 +0530

    null text input managed

--- public/script.js
+++ public/script.js
@@ -7,16 +7,23 @@ let username = prompt("Enter your name: ");
 myName.textContent = username;
 
 userInput.addEventListener("keypress", function (e) {
-  if (e.key == "Enter") {
-    let chatDiv = document.createElement("div");
-    chatDiv.classList.add("chat");
-    chatDiv.classList.add("right");
-    chatDiv.textContent = username + " : " + userInput.value;
-    chatwindow.append(chatDiv);
-    socket.emit("chat", { username, chat: userInput.value });
-    userInput.value = "";
 
-    chatwindow.scrollTop = chatwindow.scrollHeight;
+  if (e.key == "Enter") {
+    if (userInput.value) {
+      let chatDiv = document.createElement("div");
+      chatDiv.classList.add("chat");
+      chatDiv.classList.add("right");
+      chatDiv.textContent = username + " : " + userInput.value;
+
+      chatwindow.append(chatDiv);
+      socket.emit("chat", { username, chat: userInput.value });
+      userInput.value = "";
+
+      chatwindow.scrollTop = chatwindow.scrollHeight;
+
+
+    }
+
 
   }
 })
